1586a628bd4106683723d715cf4ea44a21d7ee29
Rhuan Silva De Oliveira <202503535681@alunos.estacio.br>
2025-03-25 13:14:33 +0000

    alteracao

--- xadrez.c
+++ xadrez.c
@@ -9,19 +9,34 @@ int main() {
 
     // Movimento do Bispo (5 casas na diagonal para cima e à direita)
     printf("\nMovimento do Bispo:\n");
-    int b = 0;
-    while (b < 5) {
+    int j = 0;
+    while (j < 5) {
         printf("Cima Direita\n");
-        b++;
+        j++;
     }
 
     // Movimento da Rainha (8 casas para a esquerda)
     printf("\nMovimento da Rainha:\n");
-    int r = 0;
+    int k = 0;
     do {
         printf("Esquerda\n");
-        r++;
-    } while (r < 8);
+        k++;
+    } while (k < 8);
+
+    // Movimento do Cavalo (2 casas para baixo, 1 casa para a esquerda)
+    printf("\nMovimento do Cavalo:\n");
+    
+    // Primeiro loop para mover 2 casas para baixo
+    for (int i = 0; i < 2; i++) {
+        printf("Baixo\n");
+    }
+
+    // Segundo loop para mover 1 casa para a esquerda
+    int l = 0;
+    while (l < 1) {
+        printf("Esquerda\n");
+        l++;
+    }
 
     return 0;
 }
